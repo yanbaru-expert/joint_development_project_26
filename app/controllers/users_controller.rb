212def0d28ee7cfc51b3c78a400f155755d953b7
SHINICHI <masaki.shinichi.1984@gmail.com>
2020-07-21 21:48:59 +0900

    new,create実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+  def new
+    @users = User.new
+  end
+
+  def create
+    User.create(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :age)
+  end
 end
