5d12ad82abc53c48f0c185f852b29cb1f897618d
小田尚未 <odanaomi@oda3noMacBook-Air.local>
2020-07-23 23:00:33 +0900

    編集機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,15 @@ class PostsController < ApplicationController
     Post.create(post_params)
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+  end
+
   def show
     @post = Post.find(params[:id])
   end
