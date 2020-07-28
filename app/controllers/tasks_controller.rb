6dbba4c93614cc77e016cf8414bfc351a02223d0
yuta kimura <outstandinglife718@gmail.com>
2020-07-28 19:22:03 +0900

    新規投稿機能の実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,15 @@
 class TasksController < ApplicationController
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    Task.create(task_params)
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:name, :detail)
+  end
 end
