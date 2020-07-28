d872ce16dd74e2be4489bde7bdd611d5d24c3658
yuta kimura <outstandinglife718@gmail.com>
2020-07-28 20:24:35 +0900

    編集機能の実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,6 +16,15 @@ class TasksController < ApplicationController
     Task.create(task_params)
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update(task_params)
+  end
+
   private
   def task_params
     params.require(:task).permit(:name, :detail)
